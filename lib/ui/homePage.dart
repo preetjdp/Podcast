632e73a33f21110fa6b7331a6892d72100a95f86
Preet Parekh <preetjdp123@gmail.com>
2020-06-09 13:23:27 +0530

    Added a Search bar to the top

--- lib/ui/homePage.dart
+++ lib/ui/homePage.dart
@@ -1,8 +1,11 @@
 import 'package:Podcast/resources/episodeNotifer.dart';
 import 'package:Podcast/resources/models/episode.dart';
 import 'package:Podcast/ui/abstractions/podSpinner.dart';
+import 'package:Podcast/ui/abstractions/podTheme.dart';
 import 'package:Podcast/ui/components/bottomPlayer.dart';
 import 'package:Podcast/ui/components/episodeWidget.dart';
+import 'package:Podcast/ui/components/podSearchDelegate.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:basics/basics.dart';
@@ -20,6 +23,7 @@ class HomePage extends StatelessWidget {
               : CustomScrollView(
                   physics: BouncingScrollPhysics(),
                   slivers: [
+                    SearchBar(),
                     // SliverToBoxAdapter(
                     //   child: Text(
                     //     "Featured",
@@ -65,6 +69,50 @@ class HomePageEmptyState extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: PodSpinner(),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    void _onSubmitted(String a) {
+      print("TESTING");
+      showSearch(context: context, delegate: PodSearchDelegate(), query: a);
+    }
+
+    return SliverPadding(
+      padding: EdgeInsets.only(bottom: 15),
+      sliver: SliverToBoxAdapter(
+          child: CupertinoTextField(
+        onSubmitted: _onSubmitted,
+        onChanged: (a) {
+          print(a);
+        },
+        autocorrect: false,
+        placeholder: "Search For Podcasts here",
+        textInputAction: TextInputAction.search,
+        padding: EdgeInsets.all(15),
+        clearButtonMode: OverlayVisibilityMode.always,
+        cursorRadius: PodDesign().podRadius,
+        cursorColor: PodDesign().podGrey1,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(PodDesign().podRadius)),
+        suffix: Container(
+            height: 30,
+            width: 30,
+            margin: EdgeInsets.only(right: 5),
+            decoration: BoxDecoration(
+              color: Theme.of(context).accentColor,
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Icon(
+              CupertinoIcons.search,
+              color: Colors.white,
+              size: 15,
+            )),
+      )),
     );
   }
 }
