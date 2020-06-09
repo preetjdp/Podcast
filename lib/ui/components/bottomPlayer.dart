717012fe0e511f7739b8349c55072b71bea55f4a
Preet Parekh <preetjdp123@gmail.com>
2020-06-09 16:41:39 +0530

    showBuffering logic in bottom bar

--- lib/ui/components/bottomPlayer.dart
+++ lib/ui/components/bottomPlayer.dart
@@ -1,4 +1,5 @@
 import 'package:Podcast/main.dart';
+import 'package:Podcast/ui/abstractions/podSpinner.dart';
 import 'package:Podcast/ui/components/podSearchDelegate.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/cupertino.dart';
@@ -12,50 +13,52 @@ class BottomPlayer extends StatelessWidget {
   Widget build(BuildContext context) {
     final AssetsAudioPlayer player = context.watch<AssetsAudioPlayer>();
     return Container(
-      color: Colors.white,
-      height: 80,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 80,
-            width: 80,
-            child: player.builderCurrent(
-                builder: (BuildContext context, Playing playing) {
-              if (playing.isNotNull)
-                return Image.network(playing.audio.audio.episode.thumbnail);
-              return Container();
-            }),
-          ),
-          SizedBox(
-            width: 15,
-          ),
-          Expanded(child: player.builderCurrent(
-              builder: (BuildContext context, Playing playing) {
-            if (playing.isNotNull)
-              return Text(playing.audio.audio.episode.title);
-            return Text("Select a Podcast");
-          })),
-          SizedBox(
-            width: 15,
-          ),
-          CupertinoButton(
-              color: Theme.of(context).accentColor,
-              borderRadius: BorderRadius.circular(50),
-              minSize: 20,
-              padding: EdgeInsets.all(10),
-              child: player.builderIsPlaying(
-                  builder: (BuildContext context, bool isPlaying) {
-                return isPlaying
-                    ? Icon(CupertinoIcons.pause)
-                    : Icon(CupertinoIcons.play_arrow);
-              }),
-              onPressed: player.playOrPause),
-          SizedBox(
-            width: 15,
-          ),
-        ],
-      ),
-    );
+        color: Colors.white,
+        height: 80,
+        child: player.builderRealtimePlayingInfos(
+            builder: (BuildContext context, RealtimePlayingInfos realTimeInfo) {
+          // print(realTimeInfo.toString());
+          if (realTimeInfo.isNull)
+            return Center(
+              child: Text("Start By Selecting a Podcast"),
+            );
+          return Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  height: 80,
+                  width: 80,
+                  child: realTimeInfo.current.isNotNull
+                      ? Image.network(
+                          realTimeInfo.current.audio.audio.episode.thumbnail)
+                      : Container()),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                  child: !realTimeInfo.isBuffering &&
+                          realTimeInfo.current.isNotNull
+                      ? Text(realTimeInfo.current.audio.audio.episode.title)
+                      : Center(
+                          child: PodSpinner(),
+                        )),
+              SizedBox(
+                width: 15,
+              ),
+              CupertinoButton(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.circular(50),
+                  minSize: 20,
+                  padding: EdgeInsets.all(10),
+                  child: realTimeInfo.isPlaying
+                      ? Icon(CupertinoIcons.pause)
+                      : Icon(CupertinoIcons.play_arrow),
+                  onPressed: player.playOrPause),
+              SizedBox(
+                width: 15,
+              ),
+            ],
+          );
+        }));
   }
 }
